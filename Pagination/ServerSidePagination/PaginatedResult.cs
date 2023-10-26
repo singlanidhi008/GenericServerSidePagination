112037f73ddc253108ff90cfdb0bc68ba577aa92
singlanidhi008 <139867679+singlanidhi008@users.noreply.github.com>
2023-10-26 18:36:19 +0530

    Add files via upload
    My first commit

---
+++ Pagination/ServerSidePagination/PaginatedResult.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ServerSidePagination
+{
+    public class PaginationResult<T>
+    {
+        public List<T> Items { get; set; }
+        public int CurrentPage { get; set; }
+        public int PageSize { get; set; }
+        public int TotalItems { get; set; }
+        public int TotalPages { get; set; }
+    }
+
+}
---
+++ Pagination/ServerSidePagination/PaginationHelper.cs
@@ -0,0 +1,63 @@
+﻿using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace ServerSidePagination
+{
+    public class PaginationHelper<T>
+    {
+        public static PaginationResult<T> ApplyPagination(IQueryable<T> sourceData, int currentPage, int pageSize, dynamic searchKeyword, string sortColumn, string sortOption)
+        {
+            var filteredData = sourceData;
+
+            if (searchKeyword != null)
+            {
+                filteredData = SearchProperty(filteredData, searchKeyword);
+            }
+
+            if (!string.IsNullOrEmpty(sortColumn))
+            {
+                filteredData = SortByProperty(filteredData, sortColumn, sortOption);
+            }
+
+            var totalItems = filteredData.Count();
+            int totalPages = (int)Math.Ceiling((double)totalItems / pageSize);
+
+            return new PaginationResult<T>
+            {
+                Items = filteredData.Skip((currentPage - 1) * pageSize).Take(pageSize).ToList(),
+                CurrentPage = currentPage,
+                PageSize = pageSize,
+                TotalItems = totalItems,
+                TotalPages = totalPages
+            };
+        }
+
+        private static IQueryable<T> SearchProperty(IQueryable<T> data, dynamic searchKeyword)
+        {
+            List<T> searchedData = new List<T>();
+
+            foreach (PropertyInfo prop in typeof(T).GetProperties())
+            {
+                List<T> value = data.AsEnumerable().Where(item => prop.GetValue(item) != null && prop.GetValue(item).ToString().Contains(searchKeyword, StringComparison.OrdinalIgnoreCase)).ToList();
+                foreach (var item in value)
+                {
+                    searchedData.Add(item);
+                }
+            }
+
+            return searchedData.AsQueryable();
+        }
+
+        private static IQueryable<T> SortByProperty(IQueryable<T> sourceData, string sortColumn, string sortOption)
+        {
+            var type = typeof(T);
+            ParameterExpression param = Expression.Parameter(type, "p");
+            MemberExpression property = Expression.Property(param, sortColumn);
+            LambdaExpression lambda = Expression.Lambda(property, param);
+            string methodName = sortOption.Equals("asc", StringComparison.OrdinalIgnoreCase) ? "OrderBy" : "OrderByDescending";
+            MethodCallExpression result = Expression.Call(typeof(Queryable), methodName, new Type[] { type, property.Type }, sourceData.Expression, lambda);
+            return sourceData.Provider.CreateQuery<T>(result);
+        }
+    }
+}
